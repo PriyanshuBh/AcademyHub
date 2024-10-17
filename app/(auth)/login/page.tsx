c704e8472cc3c60c3e7e28c757f6943968a351ac
PriyanshuBh <priyanshubh2003@gmail.com>
2024-10-17 23:06:21 +0530

    resetpassword

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -81,7 +81,7 @@ function Login() {
         title="Welcome Back"
         description1="Build skills for today, tomorrow, and beyond."
         description2="Education to future-proof your career."
-        image={"/public/Images/login.webp"}
+        image={"@/public/Images/login.webp"}
         formType="login"
       />
     </>
--- services/operations/authApi.ts
+++ services/operations/authApi.ts
@@ -111,6 +111,7 @@ export function login(
   router: (path: string) => void
 ) {
   return async () => {
+    console.log(" IN login api")
     const toastId = toast.loading("Loading...");
     setLoading(true);
     try {
