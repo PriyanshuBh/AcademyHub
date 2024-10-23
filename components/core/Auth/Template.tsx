1a184f2ea29fdb24023f7080e8e93b98465358d9
PriyanshuBh <priyanshubh2003@gmail.com>
2024-10-23 22:29:59 +0530

    j

--- components/core/Auth/Template.tsx
+++ components/core/Auth/Template.tsx
@@ -16,6 +16,7 @@ function Template({ title, description1, description2, image, formType } : Templ
   const { loading } = useAuthStore();
 
   return (
+    
     <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
       {loading ? (
         <div className="spinner"></div>
