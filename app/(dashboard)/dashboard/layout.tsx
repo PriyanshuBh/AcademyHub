f3941122521526ad3ab36f9f6e01e1bd55a676e7
PriyanshuBh <priyanshubh2003@gmail.com>
2024-11-18 23:42:24 +0530

    update

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from 'react'
+
+import Sidebar from '@/components/core/Dashboard/Sidebar'
+import useAuthStore from '@/store/useAuthStore'
+import useProfileStore from '@/store/useProfileStore'
+
+interface DashboardProps {
+    children: ReactNode;
+  }
+const Dashboard: React.FC<DashboardProps> = ({ children }) => {
+
+    const {loading: authLoading} =useAuthStore();
+    const {loading: profileLoading} = useProfileStore();
+
+
+
+    if(profileLoading || authLoading) {
+        return (
+            <div className='mt-10'>
+                Loading...
+            </div>
+        )
+    }
+
+
+  return (
+    <div className='relative flex bg-richblack-400'>
+        <Sidebar />
+        <div className=' flex-1 overflow-auto bg-richblack-900'>
+            <div className='py-10'>
+            {children}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
--- services/operations/authApi.ts
+++ services/operations/authApi.ts
@@ -23,41 +23,39 @@ const {
   RESETPASSWORD_API,
 } = endpoints;
 
-
 export async function sendOtp(email: string, router: (path: string) => void) {
-  const { setLoading } = useAuthStore.getState()
+  const { setLoading } = useAuthStore.getState();
   const { setProgress } = useLoadingBarStore.getState();
 
-    // const toastId = toast.loading("Loading...")
-    console.log("in send otp authapi")
-    setLoading(true);
-    try {
-      const response = await apiConnector("POST", SENDOTP_API, {
-        email,
-        checkUserPresent: true,
-      });
-      setProgress(100);
-      console.log("SENDOTP API RESPONSE............", response);
+  // const toastId = toast.loading("Loading...")
+  console.log("in send otp authapi");
+  setLoading(true);
+  try {
+    const response = await apiConnector("POST", SENDOTP_API, {
+      email,
+      checkUserPresent: true,
+    });
+    setProgress(100);
+    console.log("SENDOTP API RESPONSE............", response);
 
-      console.log(response.data.success);
+    console.log(response.data.success);
 
-      if (!response.data.success) {
-        throw new Error(response.data.message);
-      }
-
-      toast.success("OTP Sent Successfully");
-      router("/verify-email");
-    } catch (error) {
-      console.log("SENDOTP API ERROR............", error);
-      const errorMessage =
-        (error as any)?.response?.data?.message ||
-        "Something went wrong. Please try again.";
-      toast.error(errorMessage);
-      setProgress(100);
+    if (!response.data.success) {
+      throw new Error(response.data.message);
     }
-    setLoading(false);
-    // toast.dismiss(toastId)
-  ;
+
+    toast.success("OTP Sent Successfully");
+    router("/verify-email");
+  } catch (error) {
+    console.log("SENDOTP API ERROR............", error);
+    const errorMessage =
+      (error as any)?.response?.data?.message ||
+      "Something went wrong. Please try again.";
+    toast.error(errorMessage);
+    setProgress(100);
+  }
+  setLoading(false);
+  // toast.dismiss(toastId)
 }
 
 export async function signUp(
@@ -70,39 +68,40 @@ export async function signUp(
   otp: string,
   router: (path: string) => void
 ) {
-  const { setLoading } = useAuthStore.getState()
-const { setProgress } = useLoadingBarStore.getState();
- 
-    const toastId = toast.loading("Loading...");
-    setLoading(true);
-    try {
-      const response = await apiConnector("POST", SIGNUP_API, {
-        accountType,
-        firstName,
-        lastName,
-        email,
-        password,
-        confirmPassword,
-        otp,
-      });
+  const { setLoading } = useAuthStore.getState();
+  const { setProgress } = useLoadingBarStore.getState();
 
-      console.log("SIGNUP API RESPONSE............", response);
+  const toastId = toast.loading("Loading...");
+  setLoading(true);
+  try {
+    console.log("signUp apiConnector is calling");
+    const response = await apiConnector("POST", SIGNUP_API, {
+      accountType,
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      otp,
+    });
 
-      if (!response.data.success) {
-        throw new Error(response.data.message);
-      }
-      setProgress(100);
-      toast.success("Signup Successful");
-      router("/login");
-    } catch (error) {
-      setProgress(100);
-      console.log("SIGNUP API ERROR............", error);
-      toast.error("Signup Failed");
-      router("/signup");
+    console.log("SIGNUP API RESPONSE............", response);
+
+    if (!response.data.success) {
+      throw new Error(response.data.message);
     }
-    setLoading(false);
-    toast.dismiss(toastId);
-  
+    setProgress(100);
+    toast.success("Signup Successful");
+    login(email, password, router);
+    // router("/login");
+  } catch (error) {
+    setProgress(100);
+    console.log("SIGNUP API ERROR............", error);
+    toast.error("Signup Failed");
+    router("/signup");
+  }
+  setLoading(false);
+  toast.dismiss(toastId);
 }
 
 export async function login(
@@ -110,9 +109,9 @@ export async function login(
   password: string,
   router: (path: string) => void
 ) {
-  const { setLoading, setToken } = useAuthStore.getState()
-  const { setUser } = useProfileStore.getState()
-const { setProgress } = useLoadingBarStore.getState();
+  const { setLoading, setToken } = useAuthStore.getState();
+  const { setUser } = useProfileStore.getState();
+  const { setProgress } = useLoadingBarStore.getState();
 
   console.log(" IN login api function");
   const toastId = toast.loading("Loading...");
@@ -142,7 +141,7 @@ const { setProgress } = useLoadingBarStore.getState();
     setProgress(100);
     console.log("LOGIN API ERROR............", error);
     const errorMessage =
-    (error as any)?.response?.data?.message ||
+      (error as any)?.response?.data?.message ||
       "Something went wrong. Please try again.";
     toast.error(errorMessage);
   }
@@ -151,30 +150,29 @@ const { setProgress } = useLoadingBarStore.getState();
 }
 
 export async function getPasswordResetToken(email: string, setEmailSent: any) {
-  const { setLoading } = useAuthStore.getState()
- 
-    const toastId = toast.loading("Loading...");
-    setLoading(true);
-    try {
-      const response = await apiConnector("POST", RESETPASSTOKEN_API, {
-        email,
-      });
+  const { setLoading } = useAuthStore.getState();
 
-      console.log("RESETPASSTOKEN RESPONSE............", response);
+  const toastId = toast.loading("Loading...");
+  setLoading(true);
+  try {
+    const response = await apiConnector("POST", RESETPASSTOKEN_API, {
+      email,
+    });
 
-      if (!response.data.success) {
-        throw new Error(response.data.message);
-      }
+    console.log("RESETPASSTOKEN RESPONSE............", response);
 
-      toast.success("Reset Email Sent");
-      setEmailSent(true);
-    } catch (error) {
-      console.log("RESETPASSTOKEN ERROR............", error);
-      toast.error("Failed To Send Reset Email");
+    if (!response.data.success) {
+      throw new Error(response.data.message);
     }
-    toast.dismiss(toastId);
-    setLoading(false);
-  ;
+
+    toast.success("Reset Email Sent");
+    setEmailSent(true);
+  } catch (error) {
+    console.log("RESETPASSTOKEN ERROR............", error);
+    toast.error("Failed To Send Reset Email");
+  }
+  toast.dismiss(toastId);
+  setLoading(false);
 }
 
 export async function resetPassword(
@@ -183,74 +181,68 @@ export async function resetPassword(
   token: any,
   setresetComplete: any
 ) {
- 
-  const { setLoading } = useAuthStore.getState()
+  const { setLoading } = useAuthStore.getState();
 
-    const toastId = toast.loading("Loading...");
-    setLoading(true);
-    try {
-      const response = await apiConnector("POST", RESETPASSWORD_API, {
-        password,
-        confirmPassword,
-        token,
-      });
+  const toastId = toast.loading("Loading...");
+  setLoading(true);
+  try {
+    const response = await apiConnector("POST", RESETPASSWORD_API, {
+      password,
+      confirmPassword,
+      token,
+    });
 
-      console.log("RESETPASSWORD RESPONSE............", response);
+    console.log("RESETPASSWORD RESPONSE............", response);
 
-      if (!response.data.success) {
-        throw new Error(response.data.message);
-      }
-
-      toast.success("Password Reset Successfully");
-      setresetComplete(true);
-    } catch (error) {
-      console.log("RESETPASSWORD ERROR............", error);
-      toast.error("Failed To Reset Password");
+    if (!response.data.success) {
+      throw new Error(response.data.message);
     }
-    toast.dismiss(toastId);
-    setLoading(false);
-  ;
+
+    toast.success("Password Reset Successfully");
+    setresetComplete(true);
+  } catch (error) {
+    console.log("RESETPASSWORD ERROR............", error);
+    toast.error("Failed To Reset Password");
+  }
+  toast.dismiss(toastId);
+  setLoading(false);
 }
 
 export function logout(router: (path: string) => void) {
-
-    const { setToken } = useAuthStore.getState()
-    const { resetCart } = useCartStore.getState()
-    const { setUser } = useProfileStore.getState()
-    setToken(null);
-    setUser(null);
-    resetCart();
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    toast.success("Logged Out");
-    router("/");
-  ;
+  const { setToken } = useAuthStore.getState();
+  const { resetCart } = useCartStore.getState();
+  const { setUser } = useProfileStore.getState();
+  setToken(null);
+  setUser(null);
+  resetCart();
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  toast.success("Logged Out");
+  router("/");
 }
 
-export async function  forgotPassword(email: string, setEmailSent: any) {
+export async function forgotPassword(email: string, setEmailSent: any) {
+  const { setLoading } = useAuthStore.getState();
 
-  const { setLoading } = useAuthStore.getState()
+  // const toastId = toast.loading("Loading...")
+  setLoading(true);
+  try {
+    const response = await apiConnector("POST", RESETPASSTOKEN_API, {
+      email,
+    });
 
-    // const toastId = toast.loading("Loading...")
-    setLoading(true);
-    try {
-      const response = await apiConnector("POST", RESETPASSTOKEN_API, {
-        email,
-      });
+    console.log("FORGOTPASSWORD RESPONSE............", response);
 
-      console.log("FORGOTPASSWORD RESPONSE............", response);
-
-      if (!response.data.success) {
-        toast.error(response.data.message);
-        throw new Error(response.data.message);
-      }
-
-      toast.success("Reset Email Sent");
-      setEmailSent(true);
-    } catch (error) {
-      console.log("FORGOTPASSWORD ERROR............", error);
+    if (!response.data.success) {
+      toast.error(response.data.message);
+      throw new Error(response.data.message);
     }
-    // toast.dismiss(toastId)
-    setLoading(false);
-  ;
+
+    toast.success("Reset Email Sent");
+    setEmailSent(true);
+  } catch (error) {
+    console.log("FORGOTPASSWORD ERROR............", error);
+  }
+  // toast.dismiss(toastId)
+  setLoading(false);
 }
