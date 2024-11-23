472e8d03b924da40b601627161a37aa4136586e7
PriyanshuBh <priyanshubh2003@gmail.com>
2024-11-24 00:05:22 +0530

    updates

--- components/core/Auth/ProfileDropDown.tsx
+++ components/core/Auth/ProfileDropDown.tsx
@@ -1,29 +1,30 @@
-import useProfileStore from "@/store/useProfileStore"
 import { useRef, useState } from "react"
 import { AiOutlineCaretDown } from "react-icons/ai"
 import { VscDashboard, VscSignOut } from "react-icons/vsc"
+import {useRouter} from "next/navigation"
 
-import { Link, useNavigate } from "react-router-dom"
+import useOnClickOutside from "../../../hooks/useOnClickOutside"
+import { logout } from "@/services/operations/authApi"
+import useProfileStore from "@/store/useProfileStore"
+import Link from "next/link"
+interface User {
+  image: string;
+  firstName: string;
+}
+export default function ProfileDropDown() {
+  const { user }: { user: User | null } = useProfileStore();
 
+  const navigate = useRouter();
+  const [open, setOpen] = useState<boolean>(false)
+  const ref = useRef<HTMLDivElement | null>(null)
 
-// import useOnClickOutside from "../../../hooks/useOnClickOutside"
-// import { logout } from "../../../services/operations/authAPI"
+  useOnClickOutside(ref, () => setOpen(false))
 
-
-
-export default function ProfileDropdown() {
-  const { user } = useProfileStore();
-//   const dispatch = useDispatch()
-//   const navigate = useNavigate()
-  const [open, setOpen] = useState(false)
-//   const ref = useRef(null)
-
-//   useOnClickOutside(ref, () => setOpen(false))
-
-//   if (!user){
-//     console.log("no user");
-//     return localStorage.setItem("token",null)
-//   } 
+  if (!user) {
+    console.log("No user");
+    localStorage.setItem("token", "null");
+    return null;
+  }
 
   return (
     <button className="relative" onClick={() => setOpen(true)}>
@@ -39,19 +40,19 @@ export default function ProfileDropdown() {
         <div
           onClick={(e) => e.stopPropagation()}
           className="absolute top-[118%] -right-8 z-[1000] divide-y-[1px] divide-richblack-700 overflow-hidden rounded-md border-[1px] border-richblack-700 bg-richblack-800"
-        //   ref={ref}
+          ref={ref}
         >
-          <Link to="/dashboard/my-profile" onClick={() => setOpen(false)}>
+          <Link href="/dashboard/my-profile" onClick={() => setOpen(false)}>
             <div className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:bg-richblack-700 hover:text-richblack-25">
               <VscDashboard className="text-lg" />
               Dashboard
             </div>
           </Link>
           <div
-            // onClick={() => {
-            //   dispatch(logout(navigate))
-            //   setOpen(false)
-            // }}
+            onClick={() => {
+              logout(navigate.push)
+              setOpen(false)
+            }}
             className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:bg-richblack-700 hover:text-richblack-25"
           >
             <VscSignOut className="text-lg" />
