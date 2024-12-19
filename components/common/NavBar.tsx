3cdb3a26f17a1b71aa9b409c36ff50d14afaac77
PriyanshuBh <priyanshubh2003@gmail.com>
2024-12-19 22:41:44 +0530

    navbar update

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -1,7 +1,6 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-import { matchPath, RouteProps } from "react-router-dom";
 import { NavbarLinks } from "../../lib/data/navbar-links";
 import { TiShoppingCart } from "react-icons/ti";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -14,11 +13,15 @@ import ProfileDropDown from "../core/Auth/ProfileDropDown";
 import { apiConnector } from "@/services/apiConnector";
 import { categories } from "../../services/apis";
 
+import { useRouter as usenavigate } from "next/navigation";
+import useLoadingBarStore from "@/store/useLoadingBarStore";
+import { usePathname } from 'next/navigation';
+
 const NavBar = () => {
   const { token } = useAuthStore();
   const { user } = useProfileStore();
   const { totalItems } = useCartStore();
-
+  const { setProgress } = useLoadingBarStore();
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
   const [searchValue, setSearchValue] = useState("");
@@ -26,6 +29,11 @@ const NavBar = () => {
   // const matchRoutes = (routes: string  ) => {
   //     return matchPath({ path: routes }, location.pathname)
   // }
+  const navigate = usenavigate();
+  const matchRoutes = (route: string) => {
+    const  pathname  = usePathname();
+    return pathname === route ; // Simple match
+  };
   interface Sublink {
     name: string;
     link: string;
@@ -43,8 +51,8 @@ const NavBar = () => {
       console.log(result);
       localStorage.setItem("sublinks", JSON.stringify(result.data.data));
     } catch (error) {
-      // setsublinks(JSON.parse(localStorage.getItem("sublinks")));
-      // console.log("could not fetch sublinks",localStorage.getItem("sublinks"));
+      setsublinks(JSON.parse(localStorage.getItem("sublinks") ?? "[]"));
+      console.log("could not fetch sublinks", localStorage.getItem("sublinks"));
       console.log(error);
     }
   };
@@ -52,6 +60,13 @@ const NavBar = () => {
     fetchSublinks();
   }, []);
 
+  const show = useRef<any>();
+  const overlay = useRef<any>();
+  const shownav = () => {
+    show.current.classList.toggle("navshow");
+    overlay.current.classList.toggle("hidden");
+  };
+  //handeling navbar scroll
   const handleScroll = () => {
     const currentScrollPos = window.scrollY;
 
@@ -68,12 +83,12 @@ const NavBar = () => {
 
     return () => window.removeEventListener("scroll", handleScroll);
   });
-  const handelSearch = () => {
-    // e.preventDefault();
-    // if (searchValue?.length > 0) {
-    //     navigate(`/search/${searchValue}`);
-    //     setSearchValue("");
-    // }
+  const handelSearch = (e: any) => {
+    e.preventDefault();
+    if (searchValue?.length > 0) {
+      navigate.push(`/search/${searchValue}`);
+      setSearchValue("");
+    }
   };
 
   return (
@@ -81,7 +96,12 @@ const NavBar = () => {
       className={` flex sm:relative bg-richblack-900 w-screen relative z-50 h-14 items-center justify-center border-b-[1px] border-b-richblack-700 translate-y-  transition-all duration-500`}
     >
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
-        <Link href="/">
+        <Link
+          href="/"
+          onClick={() => {
+            setProgress(100);
+          }}
+        >
           <img
             src={"/Logo/Logo-Full-Light.png"}
             width={160}
@@ -90,89 +110,135 @@ const NavBar = () => {
           ></img>
         </Link>
         {/* mobile Navbar */}
-        {/* {
-                    user && user?.accountType !== "Instructor" && (
-                        <div className=' md:hidden'>
-                            <Link to='/dashboard/cart' className=' relative left-10' onClick={() => { dispatch(setProgress(100)) }} >
-                                <div className=''>
-                                    <TiShoppingCart className=' fill-richblack-25 w-8 h-8' />
-                                </div>
-                                {
-                                    totalItems > 0 && (
-                                        <span className=' font-medium text-[12px] shadow-[3px ] shadow-black bg-yellow-100 text-richblack-900 rounded-full px-[4px] absolute -top-[2px] right-[1px]'>
-                                            {totalItems}
-                                        </span>
-                                    )
-                                }
+        {user && user?.accountType !== "Instructor" && (
+          <div className=" md:hidden">
+            <Link
+              href="/dashboard/cart"
+              className=" relative left-20"
+              onClick={() => {
+                setProgress(100);
+              }}
+            >
+              <div className="">
+                <TiShoppingCart className=" fill-richblack-25 w-8 h-8" />
+              </div>
+              {totalItems > 0 && (
+                <span className=" font-medium text-[12px] shadow-[3px ] shadow-black bg-yellow-100 text-richblack-900 rounded-full px-[4px] absolute -top-[2px] right-[1px]">
+                  {totalItems}
+                </span>
+              )}
+            </Link>
+          </div>
+        )}
 
-                            </Link>
-                        </div>
-                    )
-                } */}
+        <div
+          className={`flex md:hidden  relative gap- flex-row ${
+            token !== null && user?.accountType !== "Instructor"
+              ? " -left-12"
+              : ""
+          }`}
+        >
+          <GiHamburgerMenu
+            className={`w-16 h-8 fill-richblack-25 absolute left-20 -bottom-4 `}
+            onClick={shownav}
+          />
+          <div
+            ref={overlay}
+            className=" fixed top-0 bottom-0 left-0 right-0 z-30 bg w-[100vw] hidden h-[100vh] overflow-y-hidden bg-[rgba(0,0,0,0.5)] "
+            onClick={shownav}
+          ></div>
+          <div ref={show} className="mobNav z-50">
+            <nav
+              className=" items-center flex flex-col absolute w-[200px] -left-[80px] -top-7  glass2"
+              ref={show}
+            >
+              {token == null && (
+                <Link
+                  href="/login"
+                  className=""
+                  onClick={() => {
+                    setProgress(100);
+                  }}
+                >
+                  <button
+                    onClick={shownav}
+                    className=" mt-4 text-center text-[15px] px-6 py-2 rounded-md font-semibold bg-yellow-50 text-black hover:scale-95 transition-all duration-200"
+                  >
+                    Login
+                  </button>
+                </Link>
+              )}
+              {token == null && (
+                <Link
+                  href="/signup"
+                  className="text-yellow-50"
+                  onClick={() => {
+                    setProgress(100);
+                  }}
+                >
+                  <button
+                    onClick={shownav}
+                    className="mt-4 text-center text-[15px] px-5 py-2 rounded-md font-semibold bg-yellow-50 text-black hover:scale-95 transition-all duration-200"
+                  >
+                    Signup
+                  </button>
+                </Link>
+              )}
 
-        {/* <div className={`flex md:hidden  relative gap- flex-row ${token !== null && user?.accountType !== "Instructor" ? " -left-12" : ""}`}>
-                    <GiHamburgerMenu className={`w-16 h-8 fill-richblack-25 absolute left-10 -bottom-4 `} onClick={shownav} />
-                    <div ref={overlay} className=' fixed top-0 bottom-0 left-0 right-0 z-30 bg w-[100vw] hidden h-[100vh] overflow-y-hidden bg-[rgba(0,0,0,0.5)] ' onClick={shownav}></div>
-                    <div ref={show} className='mobNav z-50'>
-                        <nav className=' items-center flex flex-col absolute w-[200px] -left-[80px] -top-7  glass2' ref={show}> */}
-        {/* {
-                                token == null && (
-                                    <Link to='/login' className='' onClick={() => { dispatch(setProgress(100)) }} >
-                                        <button onClick={shownav} className=' mt-4 text-center text-[15px] px-6 py-2 rounded-md font-semibold bg-yellow-50 text-black hover:scale-95 transition-all duration-200'>
-                                            Login
-                                        </button>
-                                    </Link>
-                                )
-                            } */}
-        {/* {
-                                token == null && (
-                                    <Link to='/signup' className='text-yellow-50' onClick={() => { dispatch(setProgress(100)) }} >
-                                        <button onClick={shownav} className='mt-4 text-center text-[15px] px-5 py-2 rounded-md font-semibold bg-yellow-50 text-black hover:scale-95 transition-all duration-200' >
-                                            Signup
-                                        </button>
-                                    </Link>
+              {token != null && (
+                <div className=" mt-2">
+                  <p className=" text-richblack-50 text-center mb-2">Account</p>
+                  {/* <Link href='/dashboard' onClick={()=>{(setProgress(100));shownav()}} className="p-2">
 
-                                )
-                            } */}
-
-        {
-          // token != null && (
-          //     <div className=' mt-2' >
-          //         <p className=' text-richblack-50 text-center mb-2'>Account</p>
-          //         <Link to='/dashboard' onClick={()=>{dispatch(setProgress(100));shownav()}} className="p-2">
-          //         {/* </Link> */}
-          //     </div>
-          // )
-        }
-        {/* <div className=' mt-4 mb-4 bg-richblack-25 w-[200px] h-[2px]'></div>
-                            <p className=' text-xl text-yellow-50 font-semibold'>Courses</p>
-                            <div className=' flex flex-col items-end pr-4'>
-                                {
-                                    sublinks?.length < 0 ? (<div></div>) : (
-                                        sublinks?.map((element, index) => (
-                                            <Link to={`/catalog/${element?.name}`} key={index} onClick={() => { dispatch(setProgress(30)); shownav() }} className="p-2 text-sm">
-                                                <p className=' text-richblack-5 '>
-                                                    {element?.name}
-                                                </p>
-                                            </Link>
-                                        )))
-                                }
-                            </div>
-                            <div className=' mt-4 mb-4 bg-richblack-25 w-[200px] h-[2px]'></div>
-                            <Link to='/about' onClick={() => { dispatch(setProgress(100)); shownav() }} className="p-2">
-                                <p className=' text-richblack-5 '>
-                                    About
-                                </p>
-                            </Link>
-                            <Link to='/contact' onClick={() => { dispatch(setProgress(100)); shownav() }} className="p-2">
-                                <p className=' text-richblack-5 '>
-                                    Contact
-                                </p>
-                            </Link>
-                        </nav>
-                    </div>
+                  </Link> */}
+                  <ProfileDropDown />
                 </div>
- */}
+              )}
+              <div className=" mt-4 mb-4 bg-richblack-25 w-[200px] h-[2px]"></div>
+              <p className=" text-xl text-yellow-50 font-semibold">Courses</p>
+              <div className=" flex flex-col items-end pr-4">
+                {sublinks?.length < 0 ? (
+                  <div></div>
+                ) : (
+                  sublinks?.map((element, index) => (
+                    <Link
+                      href={`/catalog/${element?.name}`}
+                      key={index}
+                      onClick={() => {
+                        setProgress(30);
+                        shownav();
+                      }}
+                      className="p-2 text-sm"
+                    >
+                      <p className=" text-richblack-5 ">{element?.name}</p>
+                    </Link>
+                  ))
+                )}
+              </div>
+              <div className=" mt-4 mb-4 bg-richblack-25 w-[200px] h-[2px]"></div>
+              <Link
+                href="/about"
+                onClick={() => {
+                  setProgress(100);
+                  shownav();
+                }}
+                className="p-2"
+              >
+                <p className=" text-richblack-5 ">About</p>
+              </Link>
+              <Link
+                href="/contact"
+                onClick={() => {
+                  setProgress(100);
+                  shownav();
+                }}
+                className="p-2"
+              >
+                <p className=" text-richblack-5 ">Contact</p>
+              </Link>
+            </nav>
+          </div>
+        </div>
 
         {/* Desktop Navbar */}
         <nav>
@@ -231,9 +297,15 @@ const NavBar = () => {
                 ) : (
                   <Link
                     href={element?.path || "#"}
-                    // onClick={() => { dispatch(setProgress(100)) }}
+                    onClick={() => {
+                      setProgress(100);
+                    }}
                   >
-                    <p className=" text-richblack-25 hidden md:block">
+                    <p className={`${
+                        matchRoutes(element?.path|| '')
+                          ? " text-yellow-25"
+                          : " text-richblack-25 hidden md:block"
+                      }`}>
                       {element?.title}
                     </p>
                   </Link>
@@ -266,7 +338,13 @@ const NavBar = () => {
 
         <div className="flex-row gap-5 hidden md:flex items-center">
           {user && user?.accountType !== "Instructor" && (
-            <Link href="/dashboard/cart" className=" relative px-4 ">
+            <Link
+              href="/dashboard/cart"
+              className=" relative px-4 "
+              onClick={() => {
+                setProgress(100);
+              }}
+            >
               <div className=" z-50">
                 <TiShoppingCart className=" fill-richblack-25 w-7 h-7" />
               </div>
@@ -281,7 +359,9 @@ const NavBar = () => {
             <Link
               href="/login"
               className="text-richblack-25"
-              onClick={() => {}}
+              onClick={() => {
+                setProgress(100);
+              }}
             >
               <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[7px] text-richblack-100">
                 Login
@@ -292,7 +372,9 @@ const NavBar = () => {
             <Link
               href="/signup"
               className="text-richblack-25"
-              onClick={() => {}}
+              onClick={() => {
+                setProgress(100);
+              }}
             >
               <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[7px] text-richblack-100">
                 Signup
