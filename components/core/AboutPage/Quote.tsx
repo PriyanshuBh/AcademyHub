4ea98d50649946acb54a57d9f0df9e9a504f49c0
PriyanshuBh <priyanshubh2003@gmail.com>
2025-08-12 18:43:48 +0530

    style: update Quote component styles for improved design consistency

--- components/core/AboutPage/Quote.tsx
+++ components/core/AboutPage/Quote.tsx
@@ -5,13 +5,13 @@ const Quote = () => {
   return (
     <div className=' text-xl md:text-4xl font-semibold mx-auto py-5 pb-20 text-center text-white'>
       We are passionate about revolutionizing the way we learn. Our innovative platform
-      <span className='bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text font-bold'>"combines technology</span>
-      <span className='bg-gradient-to-b from-[#FF512F] to-[#F09819] text-transparent bg-clip-text font-bold'>
+      <span className='bg-gradient-to-b from-[#1f23ff] via-[#2467f8] to-[#348ef5] text-transparent bg-clip-text font-bold'>"combines technology</span>
+      <span className='bg-gradient-to-b from-[#dc2fff] to-[#f825c3] text-transparent bg-clip-text font-bold'>
         {" "}
         expertise
       </span>
       , and community to create an 
-      <span  className='bg-gradient-to-b from-[#FF512F] to-[#F09819] text-transparent bg-clip-text font-bold'>
+      <span  className='bg-gradient-to-b from-[#b32fff] to-[#f019c1] text-transparent bg-clip-text font-bold'>
       {" "}
         unparalleled educational experience.
       </span>
