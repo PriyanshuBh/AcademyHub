0030daf1e083a8fa68b1e1275c7886a1cc62cc89
PriyanshuBh <priyanshubh2003@gmail.com>
2025-08-28 18:48:04 +0530

    style: update Button component for improved design consistency and accessibility

--- components/core/HomePage/Button.tsx
+++ components/core/HomePage/Button.tsx
@@ -22,7 +22,7 @@ const Button:React.FC<ButtonProps> = ({children,active,linkto}) => {
     <Link href={linkto}
     onClick={()=>{setProgress(100)}}  
     >
-    <div className={`text-center text-[13px] px-6 py-3 rounded-md font-semibold ${active ? "bg-yellow-50 text-black" : "bg-richblack-800"} hover:scale-95 transition-all duration-200`}>
+    <div className={`text-center text-[16px] px-6 py-2 rounded-[8px] font-semibold ${active ? "bg-white text-black" : "bg-richblack-800"} hover:scale-95 transition-all duration-200`}>
         {children}
         </div>
         </Link>
